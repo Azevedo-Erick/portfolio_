ed215670ba9bf5936b81b2dd377394bafc76d89d
Erick Azevedo <70616235+Azevedo-Erick@users.noreply.github.com>
2023-09-11 15:53:35 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://Azevedo-Erick.github.io',
-    base: '/portifolio_/',
+    base: '/portfolio_/',
 });
